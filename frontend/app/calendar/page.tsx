e9d8826d10c698ba31c908fe2ccc013016982a79
wasi <waseesomrat8@gmail.com>
2026-06-08 21:30:50 +0600

    Fix calendar styling

--- frontend/app/calendar/page.tsx
+++ frontend/app/calendar/page.tsx
@@ -1,5 +1,5 @@
 "use client";
-
+import "./calendar.css";
 import { useEffect, useState } from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
@@ -58,27 +58,63 @@ export default function CalendarPage() {
   }, []);
 
   return (
-    <div className="p-8">
-      <h1 className="text-2xl font-bold mb-4">Calendar</h1>
+    <div
+  style={{
+    minHeight: "100vh",
+    background: "#020617",
+    color: "white",
+    padding: "40px",
+  }}>
+     <h1
+  style={{
+    color: "#d8b4fe",
+    fontSize: "60px",
+    marginBottom: "20px",
+  }}
+>
+  Career Calendar
+</h1>
 
       <div className="flex gap-2 mb-4">
-        <input
-          className="border p-2"
-          placeholder="Event Title"
-          value={title}
-          onChange={(e) => setTitle(e.target.value)}
-        />
+       <input
+  placeholder="Event Title"
+  value={title}
+  onChange={(e) => setTitle(e.target.value)}
+  style={{
+    background: "#081225",
+    color: "white",
+    border: "1px solid #9333ea",
+    padding: "12px",
+    borderRadius: "10px",
+  }}
+/>
 
-        <input
-          className="border p-2"
-          type="date"
-          value={date}
-          onChange={(e) => setDate(e.target.value)}
-        />
-
-        <button className="bg-blue-500 text-white px-4" onClick={addEvent}>
-          Add
-        </button>
+     <input
+  type="date"
+  value={date}
+  onChange={(e) => setDate(e.target.value)}
+  style={{
+    background: "#081225",
+    color: "white",
+    border: "1px solid #9333ea",
+    padding: "12px",
+    borderRadius: "10px",
+  }}
+/>
+       <button
+  onClick={addEvent}
+  style={{
+    background:
+      "linear-gradient(to right,#9333ea,#c084fc)",
+    color: "white",
+    border: "none",
+    padding: "12px 20px",
+    borderRadius: "10px",
+    cursor: "pointer",
+  }}
+>
+  Add
+</button>
       </div>
 
       <FullCalendar
